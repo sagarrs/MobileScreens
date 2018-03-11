7c5d38760ec9a6e2d7cba3318c54cc9bae5b2bb7
sagar <sagar@betterhalf.ai>
2018-03-12 05:14:59 +0530

    mobile app 1st screen

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const Img1 = 'https://i.ytimg.com/vi/9D1vO-7tfZg/maxresdefault.jpg'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bodyText: 'Account Information'
+    };
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+        <Text style={styles.title}>Verify Phone Number</Text>
+        <Image style={styles.image} source={{uri: Img1}}/>
+        <Text style={styles.txt}>
+          <Text>An OTP has been sent by sms to </Text>
+          <Text style={{fontWeight: "bold"}}>+91 8892275735</Text>
+          <Text> for verification</Text>
+        </Text>
+      </View> 
     );
   }
 }
@@ -17,7 +29,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  title: {
+    color: '#0277bd',
+    fontSize: 25,
+    padding: 30
+  },
+  image: {
+    flex: 1,		
+    width: 180,		
+    marginLeft: 80,
+    // marginBottom: 300
+  },
+  txt: {
+    flex: 1,
+    paddingTop: 50,
+    paddingBottom: 150, 
+    paddingLeft: 30,
+    paddingRight: 30,
+    fontSize: 20,
+    fontWeight: "100",
+    fontFamily: 'sans-serif-thin'
+  }
 });
